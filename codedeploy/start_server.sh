16d6e75f71ff70111869e457454ddc02c1f6e8b0
TanTruong1992 <nguyenvantruong.ktx@gmail.com>
2024-08-04 15:11:58 +0700

    version3

---
+++ codedeploy/start_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# source /home/ec2-user/web/.env
+
+# $(aws ecr get-login --no-include-email --region $REGION)
+
+cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
